4680248ccaf41b98124dc46a0a991471befb3918
beratberkayg <berat.1675@gmail.com>
2023-12-27 17:03:51 +0300

    update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,23 +3,108 @@ import { dataType } from "@/types/type";
 export const data: dataType[] = [
   {
     id: "1",
-    name: "reishi",
+    name: "Kibarlı Reishi Mantarı Üçlü Komplex",
     img: "./img/reishi.png",
   },
   {
     id: "2",
-    name: "panax",
+    name: "Kibarlı Panax Ginseng",
     img: "./img/panax.png",
   },
 
   {
     id: "3",
-    name: "d1",
+    name: "Kibarlı D1",
     img: "./img/d1.png",
   },
   {
     id: "4",
-    name: "e1",
+    name: "Kibarlı E1",
     img: "./img/e1.png",
+  },
+  {
+    id: "5",
+    name: "Kibarlı Aritex",
+    img: "./img/aritex.png",
+  },
+  {
+    id: "6",
+    name: "Kibarlı Avicen",
+    img: "./img/avicen.png",
+  },
+  {
+    id: "7",
+    name: "Kibarlı G1",
+    img: "./img/g1.png",
+  },
+  {
+    id: "8",
+    name: "Kibarlı Pro Glabra",
+    img: "./img/glabra.png",
+  },
+  {
+    id: "9",
+    name: "Kibarlı H1",
+    img: "./img/h1.png",
+  },
+  {
+    id: "10",
+    name: "Kibarlı K1",
+    img: "./img/k1.png",
+  },
+  {
+    id: "11",
+    name: "Kibarlı K2",
+    img: "./img/k2.png",
+  },
+  {
+    id: "12",
+    name: "Kibarlı K3",
+    img: "./img/k3.png",
+  },
+  {
+    id: "13",
+    name: "Kibarlı K5",
+    img: "./img/k5.png",
+  },
+  {
+    id: "14",
+    name: "Kibarlı M1",
+    img: "./img/m1.png",
+  },
+  {
+    id: "15",
+    name: "Kibarlı Omadur",
+    img: "./img/omadur.png",
+  },
+  {
+    id: "16",
+    name: "Kibarlı Omaflex Complex",
+    img: "./img/omaflex.png",
+  },
+  {
+    id: "17",
+    name: "Kibarlı P1",
+    img: "./img/p1.png",
+  },
+  {
+    id: "18",
+    name: "Kibarlı S1",
+    img: "./img/e1.png",
+  },
+  {
+    id: "19",
+    name: "Kibarlı Seretan",
+    img: "./img/seretan.png",
+  },
+  {
+    id: "20",
+    name: "Kibarlı ZK",
+    img: "./img/zk.png",
+  },
+  {
+    id: "21",
+    name: "Kibarlı Cemre Ab-ı Hayat",
+    img: "./img/cemre.png",
   },
 ];
